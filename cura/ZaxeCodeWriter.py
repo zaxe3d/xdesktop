8c322d0d7f82b745ba9f927574cc50f9f7349300
Gokhan Onis <gokhan.onis@zaxe.com>
2021-10-13 17:48:23 +0300

    stl for z3+ as well and added select extruder 1 for z3+

--- cura/ZaxeCodeWriter.py
+++ cura/ZaxeCodeWriter.py
@@ -96,7 +96,7 @@ class ZaxeCodeWriter(MeshWriter):
         zipFile.write(gcodeFilePath, "data.zaxe_code")
         zipFile.write(snapshotFilePath, "snapshot.png")
         model = self._machineManager.activeMachineName.replace("+", "PLUS")
-        if model in ["Z2", "Z3"]: # only for Z2 and Z3
+        if model in ["Z2", "Z3", "Z3+"]: # only for Z2, Z3 and Z3+
             zipFile.write(modelFilePath, "model.stl")
             # os.remove(modelFilePath) # don't remove stl since it doesn't get changed regularly
         zipFile.close()
